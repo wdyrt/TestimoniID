8a07f55be2f757ac418f6532e3403d36a4ab8512
TeaR2019 <bakhtiarsulasmanto.2019@student.uny.ac.id>
2021-03-16 09:46:25 +0700

    2

--- app/src/main/java/com/bd/testimoniid/MainActivity.java
+++ app/src/main/java/com/bd/testimoniid/MainActivity.java
@@ -32,6 +32,5 @@ public class MainActivity extends AppCompatActivity {
     public void keFD2 (View view){
         pindah = new Intent(MainActivity.this, InsideCategory.class);
         startActivity(pindah);
-        finish();
     }
 }
